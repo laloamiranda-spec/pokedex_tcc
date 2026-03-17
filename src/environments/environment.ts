d250aff0123e4e9214369d05f128940fea4a9cbc
Eduardo Aguayo <lalo.amiranda@gmail.com>
2026-03-16 23:19:44 -0600

    Primer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   supabase: {
-    url: '',       // Pega aquí tu Project URL de Supabase
-    anonKey: '',   // Pega aquí tu Anon/Public Key de Supabase
+    url: 'https://iaqhxypotzqnzodjyxbz.supabase.co',       // Pega aquí tu Project URL de Supabase
+    anonKey: 'sb_publishable_KSBsERCcNVACY7cpANVemw_V2UbEoYo',   // Pega aquí tu Anon/Public Key de Supabase
   }
 };
